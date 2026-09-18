9f16bf908bea3ebf9f14a0222e785019f1dd3710
Francis Oseahumen Jossy <215143064+R-Nova@users.noreply.github.com>
2026-09-18 18:22:54 +0100

    Update axiosConfig.js

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL:'http://3.16.76.30:8080',
+    baseURL:'http://http://35.175.112.191:8080',
     headers: {
         'Content-Type': 'application/json',
     },
