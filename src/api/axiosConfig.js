e733df43b0a28649ff42c6a1081494b4f2bf430b
Francis Oseahumen Jossy <215143064+R-Nova@users.noreply.github.com>
2026-09-19 13:35:17 +0100

    Fix duplicate 'http://' in baseURL

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL:'http://http://35.175.112.191:8080',
+    baseURL:'http://35.175.112.191:8080',
     headers: {
         'Content-Type': 'application/json',
     },
